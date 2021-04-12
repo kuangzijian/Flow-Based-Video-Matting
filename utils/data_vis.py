16e40daf6cc617cd15efffe7d2183be7ed095b41
kuangzijian <kuangzijian1@hotmail.com>
2021-04-11 20:44:27 -0600

    updated output visualization

--- utils/data_vis.py
+++ utils/data_vis.py
@@ -1,24 +1,22 @@
 import cv2
 
-def plot_img_and_mask(plt, ax, img, mask, img_new_bg, i):
-    classes = mask.shape[2] if len(mask.shape) > 2 else 1
-    ax[0].clear()
-    ax[1].clear()
-    ax[2].clear()
+def plot_img_and_mask(plt, ax, img, mask, removed_bg, img_new_bg):
+    ax[0, 0].clear()
+    ax[0, 1].clear()
+    ax[1, 0].clear()
+    ax[1, 1].clear()
 
-    ax[0].set_title('Input image ' + str(i))
-    ax[0].imshow(img)
-    if classes > 1:
-        for i in range(classes):
-            ax[i+1].set_title(f'Output mask (class {i+1})')
-            ax[i+1].imshow(mask[:, :, i])
-    else:
-        ax[1].set_title(f'Output mask ' + str(i))
-        ax[1].imshow(mask)
-        ax[2].set_title(f'New Input image ' + str(i))
-        ax[2].imshow(cv2.cvtColor(img_new_bg, cv2.COLOR_BGR2RGB))
+    ax[0, 0].set_title('Input frame')
+    ax[0, 0].imshow(img)
+    ax[0, 1].set_title(f'Predicted mask')
+    ax[0, 1].imshow(mask)
+    ax[1, 0].set_title(f'Background removed')
+    ax[1, 0].imshow(cv2.cvtColor(removed_bg, cv2.COLOR_BGR2RGB))
+    ax[1, 1].set_title(f'Background replaced')
+    ax[1, 1].imshow(cv2.cvtColor(img_new_bg, cv2.COLOR_BGR2RGB))
 
     for a in ax:
-        a.set_xticks([])
-        a.set_yticks([])
+        for aa in a:
+            aa.set_xticks([])
+            aa.set_yticks([])
     plt.pause(0.001)
